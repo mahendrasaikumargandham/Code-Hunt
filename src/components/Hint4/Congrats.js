79be38d6e7534f53bc77948ea3f3b8889a1f4213
mahendrasaikumargandham <mahendragandham730@gmail.com>
2022-12-22 15:36:52 +0530

    Added Commit

--- src/components/Hint4/Congrats.js
+++ src/components/Hint4/Congrats.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import "./Congrats"
+import React from "react";
+import "./Congrats.css";
+
 function Congrats() {
   return (
-    <div className = "congrats">
-        <h1>Congratualtions! Now try to catch the killer using all 5 cases</h1>
+    <div className="congrats">
+      <h1>Congratualtions! Now try to catch the killer using all 5 cases</h1>
+      <p>TkVUSFI=</p>
     </div>
-  )
+  );
 }
 
-export default Congrats
+export default Congrats;
